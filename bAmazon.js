0ef01da800b91e2e8122905828c5f4fbac5cdb72
TheRealIzzi <israel.peck@gmail.com>
2017-11-30 16:28:54 -0600

    First upload of digital store; basic functionality ~50%

--- bAmazon.js
+++ bAmazon.js
@@ -4,6 +4,7 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("easy-table");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -11,10 +12,10 @@ var connection = mysql.createConnection({
 
 	user: "root",
 
-	password: "########",
+	password: process.env.MYSQL_ROOT_PW,
 
 	database: "bamazon_db"
-});
+})
 
 connection.connect(function(err) {
 	if (err) throw err;
@@ -32,7 +33,8 @@ function initiate() {
 			choices: [
 				"Shop (customer)",
 				"Manage (manager)",
-				"Overview (supervisor)"
+				"Overview (supervisor)",
+				"Exit"
 			]
 		})
 		.then(function(answer) {
@@ -48,6 +50,11 @@ function initiate() {
 				case "Overview (supervisor)":
 					overview();
 					break;
+
+				case "Exit":
+					console.log("\nGood day")
+					process.exit();
+					break;
 			}
 		})
 }
@@ -55,27 +62,80 @@ function initiate() {
 // ---------- Customer Section ----------
 
 function shop() {
+
+		var items = [];
+
+	connection.query("SELECT * FROM products", function(err,res) {
+		if (err) throw err;
+
+		var t = new Table 
+
+		console.log("\n")
+		res.forEach(function(product) {
+			t.cell('Product Id', product.id)
+			t.cell('Description', product.product)
+			t.cell('Price (USD)', product.price)
+			t.cell('Quantity', product.quantity)
+			t.newRow()
+		})
+		console.log(t.toString());
+		});
+
 	inquirer
 		.prompt ({
-			name: "shop",
-			type: "rawlist",
-			message: "which item would you like?"
-			choices: [
-				"Item 1"
-			]
-		})
-		.then(function(answer) {
-			switch (answer.shop) {
-				case "Item 1"
-					buy(item1);
-					break;
+			name: "purchase",
+			message: "Which product Id would you like?",
+			validate: function(value) {
+				if (isNaN(value) === false){
+					return true;
+				}
+				return false;
 			}
 		})
+		.then(function(answer) {
+			buy(answer.purchase)
+	});
 }
 
 function buy(item){
-	//
+	inquirer
+		.prompt ({
+			name: "quantity",
+			type: "input",
+			message: "How many do you want to buy?"
+		})
+		.then(function(answer) {
+				if (isNaN(answer.quantity)) {
+					console.log("Please input a number");
+					buy(item);
+				} else if (answer.quantity < 1) {
+					console.log("Please input a number");
+					buy(item);
+				} else 
+
+					console.log("\nYou purchased: "+answer.quantity+" of product #"+item)
+					console.log("\nYour purchase is ready!\n");
+					reduceItems(item, answer.quantity);
+					
+					inquirer
+						.prompt({
+							name: "next",
+							type: "string",
+							message: "Keep shopping, y/n?"
+						})
+						.then(function(answer) {
+							if (answer.next == "y") {
+								shop()
+							} else (initiate())
+						});
+			})
 }
+
+function reduceItems(item, quantity) {
+	connection.query("UPDATE products SET quantity = quantity -"+quantity+" WHERE id = "+item, function(err,res) {
+	if (err) throw err;
+	})
+}	
 
 // ---------- Manager Section ----------
 
@@ -89,7 +149,8 @@ function manage() {
 			"See product inventory",
 			"See low inventory items",
 			"Add to inventory",
-			"Add new product"
+			"Add new product",
+			"Return to main"
 			]
 		}).then(function(answer) {
 			switch (answer.manage) {
@@ -108,20 +169,90 @@ function manage() {
 				case "Add new product":
 					addProduct();
 					break;
+
+				case "Return to main":
+					initiate();
+					break;
 			}
 		});
 }
 
 function allInventory() {
-	//
+	connection.query("SELECT * FROM bamazon_db.products", function(err, res)  {
+		if (err) throw err;
+
+		var t = new Table;
+
+		res.forEach(function(product) {
+			t.cell('Product Id', product.id)
+			t.cell('Description', product.product)
+			t.cell('Price', product.price)
+			t.cell('Quantity', product.quantity)
+			t.newRow()
+		})
+
+		console.log("\n")
+		console.log(t.toString())
+		manage();
+		}
+	)
 }
 
+
 function lowInventory() {
-	//
+	connection.query("SELECT * FROM bamazon_db.products WHERE Quantity < 5", function(err, res)  {
+		if (err) throw err;
+
+		var t = new Table;
+
+		res.forEach(function(product) {
+			t.cell('Product Id', product.id)
+			t.cell('Description', product.product)
+			t.cell('Price', product.price)
+			t.cell('Quantity', product.quantity)
+			t.newRow()
+		})
+
+		console.log("\n")
+		console.log(t.toString())
+		manage();
+		}
+	)
 }
 
 function addInventory() {
-	//
+
+	inquirer
+		.prompt ({
+			name: "add_product",
+			type: "input",
+			message: "Type the name of the product you want to re-stock:"
+		})
+		.then(function(answer) {
+
+	connection.query("UPDATE bamazon_db.products SET quantity = quantity + 1 WHERE product ="+"'"+answer.add_product+"'", function(err, res)  {
+		if (err) throw err;
+
+		allInventory();
+		})
+
+
+
+		var t = new Table;
+
+		res.forEach(function(product) {
+			t.cell('Product Id', product.id)
+			t.cell('Description', product.product)
+			t.cell('Price', product.price)
+			t.cell('Quantity', product.quantity)
+			t.newRow()
+		})
+
+		console.log("\n")
+		console.log(t.toString())
+		manage();
+		}
+	)
 }
 
 function addProduct() {
